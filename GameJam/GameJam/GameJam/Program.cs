e5e43ed263e2c6785c23654293aaa885cfac88b9
Sam Connolly <sdc1g08@soton.ac.uk>
2014-11-18 23:48:41 +0000

    Working installation

--- GameJam/GameJam/GameJam/Program.cs
+++ GameJam/GameJam/GameJam/Program.cs
@@ -1,22 +1,35 @@
 using System;
+using System.Windows.Forms;
 
 namespace GameJam
 {
+
 #if WINDOWS || XBOX
+        
     static class Program
     {
 
         /// The main entry point for the application.
 
+        // check if there's an error, chuck it out if so...
+
         static void Main(string[] args)
         {
-            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+            //try
+            //{
+                AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
 
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
+                using (Game1 game = new Game1())
+                {
+                    game.Run();
+                }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Whoops! An error has occured...." + ex.ToString());
+            //}
         }
+    
 
         // do this stuff on exit
         static void OnProcessExit(object sender, EventArgs e)
